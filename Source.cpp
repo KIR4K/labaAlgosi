6015df81cc2257b5b8ad44a8e3a057452181456b
as190 <as19012005@gmail.com>
2023-10-16 02:49:49 +0300

    test

--- Source.cpp
+++ Source.cpp
@@ -1,67 +1,203 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-#include <list>
+#include <locale>
 
-using namespace :: std;
+using namespace std;
+template<typename T>
+class LinkedList {
+public:
 
-typedef struct Day {
-    string date;
-    int temperature;
+	LinkedList();
+	~LinkedList();
+	
+
+	void pop_front();
+	void push_back(T data);
+	void clear();
+	int GetSize() { return Size; };
+	T& operator[](const int index);
+	void push_front(T data);
+	void insert(T data, int index);
+
+private:
+
+
+	template<typename T>
+	class Node {
+	public:
+		Node* pNext;
+		T data;
+		Node(T data = T(), Node* pNext = nullptr) {
+			this->data = data;
+			this->pNext = pNext;
+		}
+	};
+	int Size;
+	Node<T> *head;
+
 };
 
-bool operator < (const Day& lhs, const Day& rhs) {
-    return (lhs.temperature < rhs.temperature) || (lhs.temperature == rhs.temperature && lhs.date < rhs.date);
+
+template<typename T>
+LinkedList<T>::LinkedList() 
+{
+	Size = 0;
+	head = nullptr;
 }
 
-void insertSorted(list<Day>& data, const Day& newData) {
-    auto it = data.begin();
-    while (it != data.end() && *it < newData) {
-        ++it;
-    }
-    data.insert(it, newData);
+
+template<typename T>
+LinkedList<T>::~LinkedList() 
+{
+	clear();
+}
+
+
+
+template<typename T>
+void LinkedList<T>::pop_front()
+{
+	Node<T> *temp = head;
+	head = head->pNext;
+	delete temp;
+	Size--;
+}
+
+template<typename T>
+void LinkedList<T>::push_back(T data)
+{
+	if (head == nullptr)
+	{
+		head = new Node<T>(data);
+	}
+	else
+	{
+		Node<T>* current = this->head;
+		while (current->pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node<T>(data);
+	}
+	Size++;
+}
+
+template<typename T>
+void LinkedList<T>::clear()
+{
+	while (Size)
+	{
+		pop_front();
+	}
+}
+
+template<typename T>
+T& LinkedList<T>::operator[](const int index)
+{
+	int counter = 0;
+	Node<T>* current = this->head;
+	while (current != nullptr)
+	{
+		if (counter == index)
+		{
+			return current->data;
+		}
+		current = current->pNext;
+		counter++;
+	}
+}
+
+template<typename T>
+void LinkedList<T>::push_front(T data)
+{
+	head = new Node<T>(data,head);
+	Size++;
+
+}
+
+template<typename T>
+void LinkedList<T>::insert(T data, int index)
+{
+	if (index == 0)
+	{
+		push_front(data);
+	}
+	else
+	{
+		Node<T>* previous = head;
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+		Node<T>* newNode = new Node<T>(data, previous->pNext);
+		previous->pNext = newNode;
+		Size++;
+	}
+}
+
+
+typedef struct TemperatureData {
+	string date;
+	int temperature;
+};
+
+bool operator<(const TemperatureData& lhs, const TemperatureData& rhs) {
+	return (lhs.temperature < rhs.temperature) || (lhs.temperature == rhs.temperature && lhs.date < rhs.date);
+}
+
+void insertSorted(LinkedList<TemperatureData>& data, const TemperatureData& newData) {
+	auto it = 0;
+	if (data.GetSize() == 0)
+	{
+		data.push_front(newData);
+		return;
+	}
+	while (it != data.GetSize()-1 && data[it] < newData) {
+		++it;
+	}
+	data.insert(newData,it);
 }
 
 int main() {
-    ifstream file("input.txt");
-    if (!file) {
-        cout << "Error opening file\n";
-        return 1;
-    }
+	ifstream file("input.txt");
+	if (!file) {
+		cout << "Error opening file\n";
+		return 1;
+	}
 
-    list<Day> days;
-    string line;
-    while (getline(file, line)) {
-        Day entry;
-        entry.date = line.substr(0, 10);
-        entry.temperature = stof(line.substr(11));
-        insertSorted(days, entry);
-    }
+	LinkedList<TemperatureData> data;
+	string line;
+	while (std::getline(file, line)) {
+		TemperatureData entry;
+		entry.date = line.substr(0, 10);
+		entry.temperature = stof(line.substr(11));
+		insertSorted(data, entry);
+	}
 
-    cout << "Days with temperature below zero:\n";
-    for (const auto& entry : days) {
-        if (entry.temperature < 0) {
-            cout << "Date: " << entry.date << ", Temperature: " << entry.temperature << "\n";
-        }
-    }
-
-
-    cout << "Enter temperature to search: ";
-    int searchTemperature;
-    cin >> searchTemperature;
+	cout << "Days with temperature below 0:" << endl;
+	for (int i = 0; i<data.GetSize()-1;i++) {
+		if (data[i].temperature < 0) {
+			cout << data[i].date << " - " << data[i].temperature << endl;
+		}
+	}
 
 
-    bool found = false;
-    for (const Day& day : days) {
-        if (day.temperature == searchTemperature) {
-            cout << "Day with temperature  " << searchTemperature << " found: " << day.date << endl;
-            found = true;
-        }
-    }
+	cout << "Enter a temperature to search for: ";
+	int searchTemperature;
+	cin >> searchTemperature;
 
-    if (!found) {
-        cout << "Day with temperature " << searchTemperature << " is not in list." << endl;
-    }
+	bool found = false;
+	for (int i = 0; i < data.GetSize() - 1; i++) {
+		if (data[i].temperature == searchTemperature) {
+			cout << "Day with temperature " << searchTemperature << " found: " << data[i].date << endl;
+			found = true;
+		}
+	}
 
-    return 0;
+	if (!found) {
+		cout << "Day with temperature " << searchTemperature << " not found." << endl;
+	}
+
+	return 0;
 }
