9b5e2bb1c3ea0036cc00786f09d84ad2d798e035
KIR4K <72993265+KIR4K@users.noreply.github.com>
2023-10-15 13:10:17 +0300

    Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,69 +1,68 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <algorithm>
-#include <string> 
-#include <locale> 
-using namespace ::std;
+#include <string>
+#include <list>
 
+using namespace :: std;
 
-//(структура, содержащая дату и температуру)
 typedef struct Day {
     string date;
     int temperature;
 };
 
-//  Функция сравнения(компаратор) для дальнейшего использования в функции sort
-bool compareTemperature(const Day& a, const Day& b) {
-    if (a.temperature == b.temperature) {
-        return a.date < b.date;
+bool operator < (const Day& lhs, const Day& rhs) {
+    return (lhs.temperature < rhs.temperature) || (lhs.temperature == rhs.temperature && lhs.date < rhs.date);
+}
+
+void insertSorted(list<Day>& data, const Day& newData) {
+    auto it = data.begin();
+    while (it != data.end() && *it < newData) {
+        ++it;
     }
-    return a.temperature < b.temperature;
+    data.insert(it, newData);
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-
-    vector<Day> days; //пустой вектор объектов типа Day для хранения данных
-    ifstream file("input.txt"); //Открывает файл "input.txt" для чтения данных
+    ifstream file("input.txt");
     if (!file) {
-        cout << "Ошибка при открытии файла" << endl;
+        cout << "Error opening file\n";
         return 1;
-        //Если файл не удалось открыть, выводит сообщение об ошибке и завершает программу
     }
+
+    list<Day> days;
     string line;
     while (getline(file, line)) {
-        Day day;//создаём пустой объект типа day
-        day.date = line.substr(0, 10);//записываем первые 10 символов отвечающих за дату в формате ГГГГ-ММ-ДД
-        day.temperature = stoi(line.substr(11));//оставшиеся символы записываем в температуру
-        days.push_back(day);//добавляем объект в конец вектора
+        Day entry;
+        size_t pos = line.find(' ');
+        entry.date = line.substr(0, pos);
+        entry.temperature = stof(line.substr(pos + 1));
+        insertSorted(days, entry);
     }
-    file.close();//закрываем файл
 
-    sort(days.begin(), days.end(), compareTemperature);//сортируем вектор передавая в sort третьим элементом функцию сравнения
-
-    cout << "Дни с температурой ниже нуля:" << endl;
-    for (const Day& day : days) {
-        if (day.temperature < 0) {
-            cout << day.date << " :  " << day.temperature << endl;
+    cout << "Days with temperature below zero:\n";
+    for (const auto& entry : days) {
+        if (entry.temperature < 0) {
+            cout << "Date: " << entry.date << ", Temperature: " << entry.temperature << "\n";
         }
     }
 
-    cout << "Введите значение температуры для поиска дня: ";
-    int searchTemperature;//температура которую будем искать
+
+    cout << "Enter temperature to search: ";
+    int searchTemperature;
     cin >> searchTemperature;
 
-    bool found = false;//флаг так как нету for else конструкции
+
+    bool found = false;
     for (const Day& day : days) {
         if (day.temperature == searchTemperature) {
-            cout << "День с температурой  " << searchTemperature << " найден: " << day.date << endl;
+            cout << "Day with temperature  " << searchTemperature << " found: " << day.date << endl;
             found = true;
             break;
         }
     }
-    
+
     if (!found) {
-        cout << "Дней с температурой " << searchTemperature << " не найдено." << endl;
+        cout << "Day with temperature " << searchTemperature << " is not in list." << endl;
     }
 
     return 0;
